e7626b586942a7a977e7c838cdd43f0f27f55ff6
yh <nemch.egor.fl@gmail.com>
2024-02-26 12:11:14 +0200

    Lab2 simplified

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import './App.css';
 
 
-
-import ImageOpsContainer from './Container/ImageOps';
+import ImageOpsContainer from './components/ImageOps';
 
 function App() {
   return (
